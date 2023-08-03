564deb75c294a358d6f976fd25d887541b98195c
Cipher <dipanshusaini69@gmail.com>
2023-08-04 03:12:56 +0530

    added basic list functionality and local array database

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let local_db = ["buy food", "cook food", "eat food"];
+
 app.get("/", (req, res) => {
-  res.send();
+  console.log(local_db);
+  res.render("list", { title: "Hello", lists: local_db });
+});
+
+app.post("/", (req, res) => {
+  const data = req.body.newItem;
+  // console.log(data);
+  local_db.push(data);
+  console.log(local_db);
+
+  res.redirect("/");
 });
 
 app.listen(3000, () => console.log("listening on 3000"));
